d5aa49aa784c4960772f3f107877dd41f2f79b87
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-13 16:23:13 +0200

    add content to Products Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <p>
           We started operations in 2020. We guarantee fresh produce.
           <br />
-          Save time by shopping on our app and we'll deliver the products right to your home. <br />
+          Save time by shopping on our app and we will deliver the products right to your home. <br />
           <em>We use Stripe to process your payment.</em>
         </p>
       </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,10 @@
 function Products() {
-  return <>Products page here</>;
+  return (
+    <div className="products-layout">
+      <h1>Products</h1>
+      <p>Take a look at our products</p>
+    </div>
+  );
 }
 
 export default Products;
