2735386bb54d874c4c1b3b964b7a5178bea5b4b1
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-13 21:54:37 +0200

    add new Product Details Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar.jsx';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Products from './components/Products.jsx';
+// import ProductDetails from './components/product-details/ProductDetails.jsx';
+// import ProductDetailsInfo from './components/product-details/ProductDetailsInfo.jsx';
+// import ProductDetailsNutrition from './components/product-details/ProductDetailsNutrition.jsx';
+// import ProductDetailsStorage from './components/product-details/ProductDetailsStorage.jsx';
 import Cart from './components/Cart.jsx';
 
 function App() {
@@ -37,6 +41,14 @@ function App() {
             </div>
           }
         ></Route>
+
+        {/* <Route path="/products/:id" element={<ProductDetails />}>
+          <Route path="" element={<ProductDetailsInfo />}></Route>
+
+          <Route path="nutrition" element={<ProductDetailsNutrition />}></Route>
+
+          <Route path="storage" element={<ProductDetailsStorage />}></Route>
+        </Route> */}
 
         <Route
           path="/cart"
