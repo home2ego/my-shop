e3ff59c0ce0049199d932775fe2aedcb9673e9de
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-13 22:26:05 +0200

    add Link to Product Component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
 import Button from '../ui_kit/Button.jsx';
 
 function Product({ details }) {
-  const { image, name, description, price } = details;
+  const { image, name, description, price, id } = details;
 
   return (
     <div className="product">
       <div className="product-image-container">
-        <img src={image} width="100" height="100" className="product-image" alt={name} />
+        <Link to={`${id}`}>
+          <img src={image} width="100" height="100" className="product-image" alt={name} />
+        </Link>
         <div className="product-quantity-container">
           <div className="product-quantity">0</div>
         </div>
