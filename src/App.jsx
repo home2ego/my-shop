915e0dded85de6231a63e455f6be627567b9e58a
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-13 22:05:07 +0200

    refactoring Product Details Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Navbar from './components/Navbar.jsx';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Products from './components/Products.jsx';
-// import ProductDetails from './components/product-details/ProductDetails.jsx';
-// import ProductDetailsInfo from './components/product-details/ProductDetailsInfo.jsx';
-// import ProductDetailsNutrition from './components/product-details/ProductDetailsNutrition.jsx';
-// import ProductDetailsStorage from './components/product-details/ProductDetailsStorage.jsx';
+import ProductDetails from './components/product-details/ProductDetails.jsx';
+import ProductDetailsInfo from './components/product-details/ProductDetailsInfo.jsx';
+import ProductDetailsNutrition from './components/product-details/ProductDetailsNutrition.jsx';
+import ProductDetailsStorage from './components/product-details/ProductDetailsStorage.jsx';
 import Cart from './components/Cart.jsx';
 
 function App() {
@@ -42,13 +42,13 @@ function App() {
           }
         ></Route>
 
-        {/* <Route path="/products/:id" element={<ProductDetails />}>
+        <Route path="/products/:id" element={<ProductDetails />}>
           <Route path="" element={<ProductDetailsInfo />}></Route>
 
           <Route path="nutrition" element={<ProductDetailsNutrition />}></Route>
 
           <Route path="storage" element={<ProductDetailsStorage />}></Route>
-        </Route> */}
+        </Route>
 
         <Route
           path="/cart"
