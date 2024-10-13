ac7fc18962a7a92128d358e995e2935775f759b8
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-13 22:37:31 +0200

    refactoring ProductDetails Component

--- src/components/product-details/ProductDetails.jsx
+++ src/components/product-details/ProductDetails.jsx
@@ -1,5 +1,27 @@
 function ProductDetails() {
-  return <>Product details</>;
+  return (
+    <div className="product-details-layout">
+      <div>
+        <h2>Product name here</h2>
+        <img width="125" height="125" className="product-details-image" alt="product name here" />
+      </div>
+      <div>
+        <div className="tabs">
+          <ul>
+            <li>
+              <a className="tab-active">Details</a>
+            </li>
+            <li>
+              <a>Nutrition</a>
+            </li>
+            <li>
+              <a>Storage</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductDetails;
