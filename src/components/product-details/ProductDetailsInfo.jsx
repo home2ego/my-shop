7afc541e9e4983edf1a510b3b1e551d77c61b2f3
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-14 13:03:30 +0200

    refactor ProductDetailsInfo Component

--- src/components/product-details/ProductDetailsInfo.jsx
+++ src/components/product-details/ProductDetailsInfo.jsx
@@ -1,5 +1,18 @@
+import { useOutletContext } from 'react-router-dom';
+import Button from '../../ui_kit/Button.jsx';
+
 function ProductDetailsInfo() {
-  return <>Product detail info</>;
+  const product = useOutletContext();
+  const { description, price } = product;
+
+  return (
+    <>
+      <p>
+        {description} sold at <strong>${price}</strong> per piece.
+      </p>
+      <Button>${price}</Button>
+    </>
+  );
 }
 
 export default ProductDetailsInfo;
