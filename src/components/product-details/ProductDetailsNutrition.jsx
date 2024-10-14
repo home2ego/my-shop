1dfe125a76a2e8bf0b5b9af9d942b4c0f9e43855
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-14 13:20:43 +0200

    refactor ProductDetailsNutrition Component

--- src/components/product-details/ProductDetailsNutrition.jsx
+++ src/components/product-details/ProductDetailsNutrition.jsx
@@ -1,5 +1,37 @@
+import { useOutletContext } from 'react-router-dom';
+
 function ProductDetailsNutrition() {
-  return <>Product detail nutrition</>;
+  const product = useOutletContext();
+  const { protein, carbs, fat, salt } = product.nutrition;
+
+  return (
+    <table className="table table-nutrition">
+      <thead>
+        <tr>
+          <th>Nutrient</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Protein</td>
+          <td>{protein}g</td>
+        </tr>
+        <tr>
+          <td>Carbohydrates</td>
+          <td>{carbs}g</td>
+        </tr>
+        <tr>
+          <td>Fat</td>
+          <td>{fat}g</td>
+        </tr>
+        <tr>
+          <td>Salt</td>
+          <td>{salt}g</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default ProductDetailsNutrition;
