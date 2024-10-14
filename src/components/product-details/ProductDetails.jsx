69455bfbdfa82d0fa399408a54caf613cd47436a
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-14 12:16:10 +0200

    refactor ProductDetails Component

--- src/components/product-details/ProductDetails.jsx
+++ src/components/product-details/ProductDetails.jsx
@@ -1,24 +1,43 @@
+import { useParams, NavLink, Outlet } from 'react-router-dom';
+import useSWR from 'swr';
+import Loader from '../Loader.jsx';
+
 function ProductDetails() {
+  const params = useParams();
+
+  const { data: product = {}, isLoading } = useSWR(`productinfo/id${params.id}.json`);
+  const { name, image } = product;
+
   return (
     <div className="product-details-layout">
       <div>
-        <h2>Product name here</h2>
-        <img width="125" height="125" className="product-details-image" alt="product name here" />
+        {isLoading && <Loader />}
+        <h2>{name}</h2>
+        <img src={image} width="125" height="125" className="product-details-image" alt={name} />
       </div>
+
       <div>
         <div className="tabs">
           <ul>
             <li>
-              <a className="tab-active">Details</a>
+              <NavLink className={({ isActive }) => (isActive ? 'tab-active' : '')} to="" end>
+                Details
+              </NavLink>
             </li>
             <li>
-              <a>Nutrition</a>
+              <NavLink className={({ isActive }) => (isActive ? 'tab-active' : '')} to="nutrition">
+                Nutrition
+              </NavLink>
             </li>
             <li>
-              <a>Storage</a>
+              <NavLink className={({ isActive }) => (isActive ? 'tab-active' : '')} to="storage">
+                Storage
+              </NavLink>
             </li>
           </ul>
         </div>
+
+        <Outlet context={product} />
       </div>
     </div>
   );
