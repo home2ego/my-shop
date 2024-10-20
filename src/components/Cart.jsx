82326c8498e0620d9d4821bd8117c7e77f0c22e3
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-20 11:15:34 +0200

    update Cart Component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,43 +1,11 @@
 import { useState } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
 import Input from './Input.jsx';
 import Button from './Button.jsx';
 
-const stripeLoadedPromise = loadStripe(
-  'pk_test_51HsqkCGuhXEITAut89vmc4jtjYd7XPs8hWfo2XPef15MFqI8rCFc8NqQU9WutlUBsd8kmNqHBeEmSrdMMpeEEyfT00KzeVdate'
-);
-
 function Cart({ cart }) {
-  const totalPrice = cart.reduce((total, product) => total + product.price * product.quantity, 0);
-
   const [email, setEmail] = useState('');
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-
-    const lineItems = cart.map((product) => {
-      return { price: product.price_id, quantity: product.quantity };
-    });
-
-    stripeLoadedPromise.then((stripe) => {
-      stripe
-        .redirectToCheckout({
-          lineItems: lineItems,
-          mode: 'payment',
-          successUrl: 'https://my-shop-web.netlify.app/',
-          cancelUrl: 'https://my-shop-web.netlify.app/',
-          customerEmail: email,
-        })
-        .then((response) => {
-          // this will only log if the redirect did not work
-          console.log(response.error);
-        })
-        .catch((error) => {
-          // wrong API key? you will see the error message here
-          console.log(error);
-        });
-    });
-  };
+  const totalPrice = cart.reduce((total, product) => total + product.price * product.quantity, 0);
 
   return (
     <div className="cart-layout">
@@ -53,7 +21,7 @@ function Cart({ cart }) {
                     Product
                   </th>
                   <th width="20%">Unit price</th>
-                  <th width="10%">Quanity</th>
+                  <th width="10%">Quantity</th>
                   <th width="25%">Total</th>
                 </tr>
               </thead>
@@ -82,7 +50,7 @@ function Cart({ cart }) {
               </tfoot>
             </table>
 
-            <form className="pay-form" onSubmit={handleFormSubmit}>
+            <form className="pay-form">
               <p>Enter your email and then click on pay and your products will be delivered to you on the same day!</p>
               <Input
                 placeholder="Email"
