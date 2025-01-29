edfb471b2126140e75bc2761f8be8771959a7281
Artem Zhyrnyi <ic3ego@gmail.com>
2025-01-29 13:37:26 +0200

    update photos for the About and Home components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,17 @@ function About() {
         <p>
           We started operations in 2020. We guarantee fresh produce.
           <br />
-          Save time by shopping on our app and we will deliver the products right to your home. <br />
-          <em>We use Stripe to process your payment.</em>
+          <br />
+          Save time by shopping on our app and we will deliver the products
+          right to your home.
         </p>
       </div>
       <img
-        src="https://res.cloudinary.com/deoo8jl7p/image/upload/f_auto,q_auto,w_550/v1729229826/about_i03iy5.jpg"
+        src="/src/assets/about-my-shop.webp"
         height="275"
         width="183"
         className="rounded"
-        alt=""
+        alt="A hand passing a net bag filled with fresh fruit to another person."
       />
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,19 +6,19 @@ function Home() {
       <div>
         <h1>Online shopping simplified</h1>
         <p>
-          Order your groceries from <em>MyShop</em> with our easy to use app, and get your products delivered straight
-          to your doorstep.
+          Order your groceries from <em>MyShop</em> with our easy to use app,
+          and get your products delivered straight to your doorstep.
         </p>
         <Link to="/products" className="btn btn-accent">
           Start shopping
         </Link>
       </div>
       <img
-        src="https://res.cloudinary.com/deoo8jl7p/image/upload/f_auto,q_auto,w_700/v1729229817/home_qrbfkg.jpg"
+        src="/src/assets/home-my-shop.webp"
         width="350"
-        height="240"
+        height="241"
         className="rounded"
-        alt=""
+        alt="A net bag filled with fresh tangerines and peaches lying on a table next to a stack of books."
       />
     </div>
   );
